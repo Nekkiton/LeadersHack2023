4cb9569b52cd9f87d1952ab829e3742639ead7fe
Andrey Kovanov <andrykovanov451@gmail.com>
2023-05-21 15:44:44 +0300

    add data layer

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,14 +1,19 @@
 import type { AppProps } from "next/app"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 import "antd/dist/reset.css"
 import "styles/globals.css"
 import "assets/css/index.scss"
 import AppContainer from "Application/AppContainer"
 
+const queryClient = new QueryClient()
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AppContainer>
-      <Component {...pageProps} />
-    </AppContainer>
+    <QueryClientProvider client={queryClient}>
+      <AppContainer>
+        <Component {...pageProps} />
+      </AppContainer>
+    </QueryClientProvider>
   )
 }
