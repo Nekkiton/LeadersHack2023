e6e1f620b885079559b845d2cd61ede1defc7276
legbl4 <legbl4.ivn@yandex.ru>
2023-05-26 01:43:50 +0400

    [backend]: Schema migrations

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import configuration from 'config/configuration';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { InternalController } from './_internal/_internal.controller';
+import { CreateUserTable1685050336357 } from './migrations/1685050336357-CreateUserTable';
 
 @Module({
   imports: [
@@ -16,23 +17,18 @@ import { InternalController } from './_internal/_internal.controller';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        console.log(`TEST TEST: ${configService.get('database.host')}`);
-        console.log(`TEST TEST: ${configService.get('database.port')}`);
-        console.log(`TEST TEST: ${configService.get('database.username')}`);
-        console.log(`TEST TEST: ${configService.get('database.password')}`);
-        console.log(`TEST TEST: ${configService.get('database.database')}`);
-        return {
-          type: 'postgres',
-          host: configService.get('database.host'),
-          port: configService.get('database.port'),
-          username: configService.get('database.username'),
-          password: configService.get('database.password'),
-          database: configService.get('database.database'),
-          autoLoadEntities: true,
-          logging: 'all',
-        };
-      },
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.database'),
+        autoLoadEntities: true,
+        migrationsRun: true,
+        migrations: [CreateUserTable1685050336357],
+        logging: 'all',
+      }),
     }),
   ],
   controllers: [InternalController],
