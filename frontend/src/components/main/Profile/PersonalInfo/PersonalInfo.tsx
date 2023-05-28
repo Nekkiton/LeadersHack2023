bd1c1c0b13ef317cb3e226f24d6ad43d35510c47
Andrey Kovanov <andrykovanov451@gmail.com>
2023-05-28 19:15:48 +0300

    fix birthday

--- frontend/src/components/main/Profile/PersonalInfo/PersonalInfo.tsx
+++ frontend/src/components/main/Profile/PersonalInfo/PersonalInfo.tsx
@@ -64,7 +64,7 @@ export default function PersonalInfo() {
           const { birthday, ...restValues } = values ?? {}
           updateProfile.mutate({
             ...restValues,
-            birthday: birthday?.toISOString(),
+            birthday: birthday?.format("YYYY-MM-DD"),
           })
         }}
         form={form}
